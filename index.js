b711ff5eb47729271e1b714451ae5e583f6b143f
Sumanta <nandisumanta0@gmail.com>
2015-12-23 09:06:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 var loki = require('lokijs');
 var bodyParser = require('body-parser');
@@ -11,22 +11,22 @@ app.use(bodyParser.json());
 var db = new loki('loki.json');
 var children = db.addCollection('children');
 
-app.set('port', (process.env.PORT || 3000))
+app.set('port', (process.env.PORT || 3000));
 
 app.post('/', function(request, response) {
     console.log(request.body);
-  children.insert({name:request.body}) 
+  children.insert({name:request.body}); 
   response.send('Hello World!');
-})
+});
 
 app.get('/', function(request, response) {
   var y = children.get(1); 
   console.log(y);
   response.send(y);
-})
+});
 
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
 
